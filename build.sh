05120934c801f17d1d5138f506e52bcc76156461
un-simp <uncraftablecreeper@gmail.com>
2023-08-18 02:14:49 +0100

    Fri 18 Aug 02:14:49 BST 2023

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@ dpkg-scanpackages --arch amd64 pool/ > dists/stable/main/binary-amd64/Packages
 cat dists/stable/main/binary-amd64/Packages | gzip -9 > dists/stable/main/binary-amd64/Packages.gz
 
 # Release, Release.gpg & InRelease
-apt-ftparchive release . > Release
-gpg --default-key "jack@amphe.co.uk" -abs -o - Release > Release.gpg
-gpg --default-key "jack@amphe.co.uk" --clearsign -o - Release > InRelease
+apt-ftparchive dists/stable/main/binary-amd64/release . > dists/stable/main/binary-amd64/release
+gpg --default-key "jack@amphe.co.uk" -abs -o - dists/stable/main/binary-amd64/Release > dists/stable/main/binary-amd64/Release.gpg
+gpg --default-key "jack@amphe.co.uk" --clearsign -o - dists/stable/main/binary-amd64/Release > dists/stable/main/binary-amd64/InRelease
 
 # Commit & push
 git add -A
