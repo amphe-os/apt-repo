ef2560a00eb70ccf9490bcc9cb28281fb4a6d02f
un-simp <uncraftablecreeper@gmail.com>
2023-08-18 02:57:21 +0100

    Fri 18 Aug 02:57:21 BST 2023

--- build.sh
+++ build.sh
@@ -1,13 +1,8 @@
-# Packages & Packages.gz
-dpkg-scanpackages --arch amd64 pool/ > dists/stable/main/binary-amd64/Packages
-cat dists/stable/main/binary-amd64/Packages | gzip -9 > dists/stable/main/binary-amd64/Packages.gz
-
-# Release, Release.gpg & InRelease
-apt-ftparchive release dists/stable/main/binary-amd64 . > dists/stable/Release
-gpg --default-key "jack@amphe.co.uk" -abs -o - dists/stable/Release > dists/stable/Release.gpg
-gpg --default-key "jack@amphe.co.uk" --clearsign -o - dists/stable/Release > dists/stable/InRelease
-
+echo "enter path to deb package to add"
+read var
+reprepro -b . includedeb stable $var
 # Commit & push
-#git add -A
-#git commit -m "$(date)"
-#git push
+git add -A
+git commit -m "$(date)"
+git push
+
